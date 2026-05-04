024a9ded2b3627711f01227df18cb2a6142223b9
Mo <momo.tiltis@gmail.com>
2026-05-04 13:04:11 +0000

    update css von navbar und fix sonderangebote.js

--- sonderangebote.js
+++ sonderangebote.js
@@ -7,11 +7,14 @@ async function renderSonderangebote() {
 
   produkte.forEach((produkt, index) => {
     const produktElement = `
-        <div class="produkt" id="produkt-${produkt.id}" style="opacity: 1; animation: none;" onClick="window.location.href='produkt-kaufen.html?id=${produkt.id}'">
+        <div class="produkt" id="produkt-${produkt.id}" style="animation-delay: ${index * 0.1}s;" onAnimationEnd="this.style.opacity = '1';" onClick="window.location.href='produkt-kaufen.html?id=${produkt.id}'">
           <img src="${produkt["image-url"]}" alt="${produkt.name}" />
           <h2 class="produkt-name">${produkt.name}</h2>
           <p class="produkt-description">${produkt.description}</p>
-          <p class="produkt-preis">${produkt.price.toFixed(2)} €</p>
+          <p class="produkt-preis" style="text-decoration: ${produkt.reduced ? "line-through" : "none"}; ${produkt.reduced ? "color: red; margin-bottom: 0; " : ""}">
+            ${produkt.price.toFixed(2)} €
+          </p>
+          ${produkt.reduced ? `<p class="produkt-reducedPreis">nur ${produkt["reduced-price"].toFixed(2)} €</p>` : ""}
         </div>
       `;
 
